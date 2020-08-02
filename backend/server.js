6583c8a6487c1ba4f4dde9d60925a473c7495781
naser01 <nnkilani@live.com>
2020-08-02 14:49:19 +0300

    1

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (!dev) {
   app.use('*', express.static(path.resolve(__dirname, '..', 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.send(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.send(path.resolve(__dirname, '..','client', 'build', 'index.html'))
   })
 }
 
