05b5c812c72cc50dcd69ce2ff4a9b9a3a0cc07c7
naser01 <nnkilani@live.com>
2020-08-02 14:03:31 +0300

    1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
@@ -10,7 +10,7 @@ const dev = app.get('env') !== 'production';
 const normalizePort = port => parseInt(port, 10);
 const PORT = normalizePort(process.env.PORT || 5000);
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
@@ -56,25 +56,7 @@ const movieRoute = require('./routes/movies')
 app.use('/account', signupRoute)
 app.use('/movies', movieRoute)
 
+const server = createServer(app);
 app.listen(PORT, () => console.log(`Server is starting on PORT ${PORT}`));
 
 
-
-/**
-app.get('*', (req, res) => {
-  res.send(path.join(__dirname, '..', 'client', 'public', 'index.html'))
-})*/
-
-
-/**
-if (!dev) {
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
-  app.use('*', express.static(path.resolve(__dirname, '../client/build')));
-  app.get('*', (res, req) => {
-    res.send(path.resolve(__dirname, 'build', 'client/build/index.html'))
-  })
-}
-
-if (dev) {
-  app.use(morgan('dev'))
-}*/
